bf01cfb0c55e4844923995815bc72bb53fedf697
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2012-03-05 13:51:52 +0530

    Put the test classes in the correct module

--- test/go_api_client/pipeline_test.rb
+++ test/go_api_client/pipeline_test.rb
@@ -1,25 +1,27 @@
 require "test_helper"
 
-class PipelineTest < Test::Unit::TestCase
+module GoApiClient
+  class PipelineTest < Test::Unit::TestCase
 
-  def setup()
-    stub_request(:get, "http://localhost:8153/go/api/pipelines/defaultPipeline/1.xml").to_return(:body => file_contents("pipelines_1.xml"))
-  end
+    def setup
+      stub_request(:get, "http://localhost:8153/go/api/pipelines/defaultPipeline/1.xml").to_return(:body => file_contents("pipelines_1.xml"))
+    end
 
-  def test_fetch_populates_necessary_fields
-    link = "http://localhost:8153/go/api/pipelines/defaultPipeline/1.xml"
-    pipeline = GoApiClient::Pipeline.new(link)
-    pipeline.fetch
+    def test_fetch_populates_necessary_fields
+      link = "http://localhost:8153/go/api/pipelines/defaultPipeline/1.xml"
+      pipeline = GoApiClient::Pipeline.new(link)
+      pipeline.fetch
 
-    assert_equal "1", pipeline.label
-    assert_equal ["Update README"], pipeline.commit_messages
-  end
+      assert_equal "1", pipeline.label
+      assert_equal ["Update README"], pipeline.commit_messages
+    end
 
-  def test_pipeline_instance_identified_by_details_link
-    link = "http://localhost:8153/go/api/pipelines/defaultPipeline/1.xml"
-    pipeline = GoApiClient::Pipeline.new(link)
+    def test_pipeline_instance_identified_by_details_link
+      link = "http://localhost:8153/go/api/pipelines/defaultPipeline/1.xml"
+      pipeline = GoApiClient::Pipeline.new(link)
 
-    assert pipeline.same?(link)
-    assert(false == pipeline.same?("http://localhost:8153/go/api"))
+      assert pipeline.same?(link)
+      assert(false == pipeline.same?("http://localhost:8153/go/api"))
+    end
   end
 end
--- test/go_api_client/stage_test.rb
+++ test/go_api_client/stage_test.rb
@@ -1,32 +1,34 @@
 require "test_helper"
 
-class StageTest < Test::Unit::TestCase
+module GoApiClient
+  class StageTest < Test::Unit::TestCase
 
-  def setup()
-    stub_request(:get, "http://localhost:8153/go/api/pipelines/defaultPipeline/stages.xml").to_return(:body => file_contents("stages.xml"))
-    stub_request(:get, "http://localhost:8153/go/api/pipelines/defaultPipeline/1.xml").to_return(:body => file_contents("pipelines_1.xml"))
-    stub_request(:get, "http://localhost:8153/go/api/stages/1.xml").to_return(:body => file_contents("stages_1.xml"))
-    stub_request(:get, "http://localhost:8153/go/api/stages/2.xml").to_return(:body => file_contents("stages_2.xml"))
-    stub_request(:get, "http://localhost:8153/go/api/jobs/1.xml").to_return(:body => file_contents("jobs_1.xml"))
-    stub_request(:get, "http://localhost:8153/go/api/jobs/2.xml").to_return(:body => file_contents("jobs_2.xml"))
-  end
-
-  def test_stage
-    pipelines = GoApiClient.runs("localhost:8153")
-    stages = pipelines.first.stages
-    
-    assert_equal 1, pipelines.count
-    assert_equal 2, stages.count
-    assert_equal "oogabooga <twgosaas@gmail.com>", pipelines.first.authors
-    stages.each do |stage|
-      assert_equal "oogabooga <twgosaas@gmail.com>", stage.authors.first.name
-      assert_equal "Failed", stage.result
+    def setup
+      stub_request(:get, "http://localhost:8153/go/api/pipelines/defaultPipeline/stages.xml").to_return(:body => file_contents("stages.xml"))
+      stub_request(:get, "http://localhost:8153/go/api/pipelines/defaultPipeline/1.xml").to_return(:body => file_contents("pipelines_1.xml"))
+      stub_request(:get, "http://localhost:8153/go/api/stages/1.xml").to_return(:body => file_contents("stages_1.xml"))
+      stub_request(:get, "http://localhost:8153/go/api/stages/2.xml").to_return(:body => file_contents("stages_2.xml"))
+      stub_request(:get, "http://localhost:8153/go/api/jobs/1.xml").to_return(:body => file_contents("jobs_1.xml"))
+      stub_request(:get, "http://localhost:8153/go/api/jobs/2.xml").to_return(:body => file_contents("jobs_2.xml"))
     end
-    assert_equal "Acceptance", stages.first.name
-    assert_equal "Units", stages.last.name
-    assert_equal ["Update README"], pipelines.first.commit_messages
 
-    assert_equal "http://localhost:8153/go/files/defaultPipeline/1/Acceptance/1/Test/cruise-output/console.log", stages.first.jobs.first.console_log_url
-    assert_equal "http://localhost:8153/go/files/defaultPipeline/1/Units/1/Test/cruise-output/console.log", stages.last.jobs.first.console_log_url
+    def test_stage
+      pipelines = GoApiClient.runs("localhost:8153")
+      stages = pipelines.first.stages
+
+      assert_equal 1, pipelines.count
+      assert_equal 2, stages.count
+      assert_equal "oogabooga <twgosaas@gmail.com>", pipelines.first.authors
+      stages.each do |stage|
+        assert_equal "oogabooga <twgosaas@gmail.com>", stage.authors.first.name
+        assert_equal "Failed", stage.result
+      end
+      assert_equal "Acceptance", stages.first.name
+      assert_equal "Units", stages.last.name
+      assert_equal ["Update README"], pipelines.first.commit_messages
+
+      assert_equal "http://localhost:8153/go/files/defaultPipeline/1/Acceptance/1/Test/cruise-output/console.log", stages.first.jobs.first.console_log_url
+      assert_equal "http://localhost:8153/go/files/defaultPipeline/1/Units/1/Test/cruise-output/console.log", stages.last.jobs.first.console_log_url
+    end
   end
 end
