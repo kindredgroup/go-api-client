bbb8551ca3cebafff95af9f55804c3df09f822f3
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2012-05-07 16:03:17 +0530

    Fix code covergae excludes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,17 @@
+require 'rubygems'
+require 'bundler/setup'
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/.bundle/"
+  add_filter "/bundle/"
+  add_filter "/test/"
+end
 
+Bundler.require
 require 'test/unit'
 require 'go_api_client'
 require 'ostruct'
-Dir["go_api_client/**/*.rb"].each {|f| require f}
+# Dir["go_api_client/**/*.rb"].each {|f| require f}
 require 'webmock/test_unit'
 
 class Test::Unit::TestCase
