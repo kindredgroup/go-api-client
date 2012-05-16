37bd02e6b19a52bfd3f5237999801d646b1adab9
Ketan Padegaonkar and Manish Chakravarty <go-saas+ketan+manish@thoughtworks.com>
2012-05-16 14:36:32 +0530

    Rename the details_link to url

--- lib/go_api_client/pipeline.rb
+++ lib/go_api_client/pipeline.rb
@@ -1,14 +1,14 @@
 module GoApiClient
   class Pipeline
-    attr_accessor :details_link, :id, :commits, :label, :authors, :stages
+    attr_accessor :url, :id, :commits, :label, :authors, :stages
 
-    def initialize(details_link)
-      @details_link = details_link
+    def initialize(url)
+      @url = url
       @stages = []
     end
 
     def fetch
-      doc = Nokogiri::XML(open(self.details_link))
+      doc = Nokogiri::XML(open(self.url))
       @label = doc.root.attributes["label"].value
       @id = doc.root.xpath("./id").first.content
       @commits = doc.root.xpath("./materials/material/modifications/changeset").collect do |changeset|
