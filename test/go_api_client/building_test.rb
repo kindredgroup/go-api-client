4a05625e28094cea4748928d665e0652f5897d12
Nikhil Mungel <mail@hyfather.com>
2012-06-06 01:12:14 +0530

    Refactoring specs

--- test/go_api_client/building_test.rb
+++ test/go_api_client/building_test.rb
@@ -3,21 +3,25 @@ require "test_helper"
 module GoApiClient
   class BuildingTest < Test::Unit::TestCase
     def setup
-      stub_request(:get, "http://go-server.1.project:8153/go/api/pipelines/defaultPipeline/1.xml").to_return(:body => file_contents("building/pipeline_1.xml"))
-      stub_request(:get, "http://go-server.1.project:8153/go/api/pipelines/defaultPipeline/2.xml").to_return(:body => file_contents("building/pipeline_2.xml"))
-      stub_request(:get, "http://go-server.1.project:8153/go/api/pipelines/defaultPipeline/3.xml").to_return(:body => file_contents("building/pipeline_3.xml"))
+      url_to_xml_file_mapping = {
+        "api/pipelines/defaultPipeline/1.xml" => "building/pipeline_1.xml",
+        "api/pipelines/defaultPipeline/2.xml" => "building/pipeline_2.xml",
+        "api/pipelines/defaultPipeline/3.xml" => "building/pipeline_3.xml",
+        "api/stages/1.xml" => "building/stages_1.xml",
+        "api/stages/2.xml" => "building/stages_2.xml",
+        "api/stages/3.xml" => "building/stages_3.xml",
+        "api/stages/4.xml" => "building/stages_4.xml",
+        "api/jobs/1.xml" => "building/job_1.xml",
+        "api/jobs/2.xml" => "building/job_2.xml",
+        "api/jobs/3.xml" => "building/job_3.xml",
+        "api/jobs/4.xml" => "building/job_4.xml",
+        "api/pipelines/defaultPipeline/stages.xml" => "building/stages.xml"
+      }
 
-      stub_request(:get, "http://go-server.1.project:8153/go/api/stages/1.xml").to_return(:body => file_contents("building/stages_1.xml"))
-      stub_request(:get, "http://go-server.1.project:8153/go/api/stages/2.xml").to_return(:body => file_contents("building/stages_2.xml"))
-      stub_request(:get, "http://go-server.1.project:8153/go/api/stages/3.xml").to_return(:body => file_contents("building/stages_3.xml"))
-      stub_request(:get, "http://go-server.1.project:8153/go/api/stages/4.xml").to_return(:body => file_contents("building/stages_4.xml"))
-
-      stub_request(:get, "http://go-server.1.project:8153/go/api/jobs/1.xml").to_return(:body => file_contents("building/job_1.xml"))
-      stub_request(:get, "http://go-server.1.project:8153/go/api/jobs/2.xml").to_return(:body => file_contents("building/job_2.xml"))
-      stub_request(:get, "http://go-server.1.project:8153/go/api/jobs/3.xml").to_return(:body => file_contents("building/job_3.xml"))
-      stub_request(:get, "http://go-server.1.project:8153/go/api/jobs/4.xml").to_return(:body => file_contents("building/job_4.xml"))
-      
-      stub_request(:get, "http://go-server.1.project:8153/go/api/pipelines/defaultPipeline/stages.xml").to_return(:body => file_contents("building/stages.xml"))
+      url_to_xml_file_mapping.each do |url, filename|
+        stub_request(:get, "http://go-server.1.project:8153/go/#{url}").
+          to_return(:body => file_contents(filename))  
+      end
     end
 
     test "should tell that a build is done if all stages have completed" do
