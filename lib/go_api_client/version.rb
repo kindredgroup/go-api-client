318e216953bf01328f0e0d0a60dd2cc6e47ce308
Shishir Das <shishir.das@gmail.com>
2012-06-19 19:36:09 +0530

    bumped up the minor version

--- lib/go_api_client/version.rb
+++ lib/go_api_client/version.rb
@@ -1,8 +1,8 @@
 module GoApiClient
   module Version
     MAJOR = 0
-    MINOR = 0
-    PATCH = 10
+    MINOR = 1
+    PATCH = 0
     RELEASE = ENV['GO_PIPELINE_COUNTER']
     VERSION = [MAJOR, MINOR, PATCH, RELEASE].compact.join('.')
   end
