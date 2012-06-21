d68ec31fb113a04211f424b90bd49e1ef1e00f92
Shishir Das <shishir.das@gmail.com>
2012-06-21 13:52:47 +0530

    Add a test to parse out job properties

--- lib/go_api_client/job.rb
+++ lib/go_api_client/job.rb
@@ -2,6 +2,19 @@ module GoApiClient
   class Job
     attr_accessor :artifacts_uri, :console_log_url, :url, :http_fetcher
 
+    PROPERTIES = {
+      :duration   => :cruise_job_duration,
+      :result     => :cruise_job_result,
+      :scheduled  => :cruise_timestamp_01_scheduled,
+      :assigned   => :cruise_timestamp_02_assigned,
+      :preparing  => :cruise_timestamp_03_preparing,
+      :building   => :cruise_timestamp_04_building,
+      :completing => :cruise_timestamp_05_completing,
+      :completed  => :cruise_timestamp_06_completed,
+    }
+
+    attr_accessor *PROPERTIES.keys
+
     include GoApiClient::Helpers::SimpleAttributesSupport
 
     def initialize(root, attributes={})
@@ -20,6 +33,18 @@ module GoApiClient
     def parse!
       self.artifacts_uri = @root.xpath("./artifacts").first.attributes["baseUri"].value
       self.url           = href_from(@root.xpath("./link[@rel='self']"))
+
+      PROPERTIES.each do |variable, property_name|
+        property_value = @root.xpath("./properties/property[@name='#{property_name}']").first.content
+
+        if property_name =~ /timestamp/
+          property_value = Time.parse(property_value).utc
+        elsif property_value =~ /^\d+$/
+          property_value = property_value.to_i
+        end
+
+        self.send("#{variable}=", property_value)
+      end
 
       @root = nil
       self
--- test/go_api_client/job_test.rb
+++ test/go_api_client/job_test.rb
@@ -13,6 +13,16 @@ module GoApiClient
       assert_equal 'http://localhost:8153/go/api/jobs/1.xml', job.url
       assert_equal 'http://localhost:8153/go/files/defaultPipeline/1/Units/1/Test', job.artifacts_uri
       assert_equal 'http://localhost:8153/go/files/defaultPipeline/1/Units/1/Test/cruise-output/console.log', job.console_log_url
+
+      assert_equal 900, job.duration
+      assert_equal "Failed", job.result
+
+      assert_equal Time.parse('2012-02-23 11:46:15 UTC'), job.scheduled
+      assert_equal Time.parse('2012-02-23 11:46:27 UTC'), job.assigned
+      assert_equal Time.parse('2012-02-23 11:46:37 UTC'), job.preparing
+      assert_equal Time.parse('2012-02-23 11:46:41 UTC'), job.building
+      assert_equal Time.parse('2012-02-23 11:46:42 UTC'), job.completing
+      assert_equal Time.parse('2012-02-23 11:46:45 UTC'), job.completed
     end
 
   end
