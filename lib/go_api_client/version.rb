71e1e84f7e205e2a9dff9934377c0b900c038b36
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2013-02-16 08:50:11 +0530

    Bump version to v0.3.0

--- lib/go_api_client/version.rb
+++ lib/go_api_client/version.rb
@@ -1,7 +1,7 @@
 module GoApiClient
   module Version
     MAJOR = 0
-    MINOR = 2
+    MINOR = 3
     PATCH = 0
     RELEASE = ENV['GO_PIPELINE_COUNTER']
     VERSION = [MAJOR, MINOR, PATCH, RELEASE].compact.join('.')
