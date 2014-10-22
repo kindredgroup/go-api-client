2fdb3f1e4eb866fe971e28dc20952d7743cc3452
Ilja Bobkevic <ilja.bobkevic@unibet.com>
2014-10-22 12:13:15 +0200

    Format http fetcher class

--- lib/go_api_client/http_fetcher.rb
+++ lib/go_api_client/http_fetcher.rb
@@ -9,6 +9,7 @@ module GoApiClient
 
     class HttpError < ConnectionError
       attr_reader :http_code
+
       def initialize(msg, http_code)
         super(msg)
         @http_code = http_code
@@ -16,23 +17,23 @@ module GoApiClient
     end
 
     NET_HTTP_EXCEPTIONS = [
-      EOFError,
-      Errno::ECONNABORTED,
-      Errno::ECONNREFUSED,
-      Errno::ECONNRESET,
-      Errno::ECONNRESET, EOFError,
-      Errno::EINVAL,
-      Errno::ETIMEDOUT,
-      Net::HTTPBadResponse,
-      Net::HTTPClientError,
-      Net::HTTPError,
-      Net::HTTPFatalError,
-      Net::HTTPHeaderSyntaxError,
-      Net::HTTPRetriableError,
-      Net::HTTPServerException,
-      Net::ProtocolError,
-      SocketError,
-      Timeout::Error,
+        EOFError,
+        Errno::ECONNABORTED,
+        Errno::ECONNREFUSED,
+        Errno::ECONNRESET,
+        Errno::ECONNRESET, EOFError,
+        Errno::EINVAL,
+        Errno::ETIMEDOUT,
+        Net::HTTPBadResponse,
+        Net::HTTPClientError,
+        Net::HTTPError,
+        Net::HTTPFatalError,
+        Net::HTTPHeaderSyntaxError,
+        Net::HTTPRetriableError,
+        Net::HTTPServerException,
+        Net::ProtocolError,
+        SocketError,
+        Timeout::Error,
     ]
 
     NET_HTTP_EXCEPTIONS << OpenSSL::SSL::SSLError if defined?(OpenSSL)
@@ -77,64 +78,68 @@ module GoApiClient
     private
 
 
-      def get(url, limit = 10)
-        raise ArgumentError, 'HTTP redirect too deep' if limit == 0
-        uri = URI.parse(url)
+    def get(url, limit = 10)
+      raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+      uri = URI.parse(url)
 
-        password = @options[:password] || uri.password
-        username = @options[:username] || uri.user
-        params   = @options[:params]   || {}
+      password = @options[:password] || uri.password
+      username = @options[:username] || uri.user
+      params = @options[:params] || {}
 
-        uri.query = URI.encode_www_form(params) if params.any?
+      uri.query = URI.encode_www_form(params) if params.any?
 
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = uri.scheme == 'https'
-        http.verify_mode = @options[:ssl_verify_mode] == 0? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == 'https'
+      http.verify_mode = @options[:ssl_verify_mode] == 0 ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
 
 
-        response = http.start do |http|
-          req = Net::HTTP::Get.new(uri.request_uri)
-          req.basic_auth(username, password) if username || password
-          http.request(req)
-        end
-
-        case response
-          when Net::HTTPRedirection then @response = get(response['location'], limit - 1)
-          else @response = response
-        end
-
-        @response
-      end
-
-      def post(url, limit = 10)
-        raise ArgumentError, 'HTTP redirect too deep' if limit == 0
-        uri = URI.parse(url)
-
-        password = @options[:password] || uri.password
-        username = @options[:username] || uri.user
-        params   = @options[:params]   || {}
-        headers  = @options[:headers]  || {}
-
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = uri.scheme == 'https'
-
-        req = Net::HTTP::Post.new(uri.request_uri)
-
-        headers.each do |header_name, value|
-          req[header_name] = value
-        end
-
+      response = http.start do |http|
+        req = Net::HTTP::Get.new(uri.request_uri)
         req.basic_auth(username, password) if username || password
-
-        req.set_form_data(params)
-        response = http.request(req)
-
-        case response
-          when Net::HTTPRedirection then @response = post(response['location'], limit - 1)
-          else @response = response
-        end
-
-        @response
+        http.request(req)
       end
+
+      case response
+        when Net::HTTPRedirection then
+          @response = get(response['location'], limit - 1)
+        else
+          @response = response
+      end
+
+      @response
+    end
+
+    def post(url, limit = 10)
+      raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+      uri = URI.parse(url)
+
+      password = @options[:password] || uri.password
+      username = @options[:username] || uri.user
+      params = @options[:params] || {}
+      headers = @options[:headers] || {}
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == 'https'
+
+      req = Net::HTTP::Post.new(uri.request_uri)
+
+      headers.each do |header_name, value|
+        req[header_name] = value
+      end
+
+      req.basic_auth(username, password) if username || password
+
+      req.set_form_data(params)
+      response = http.request(req)
+
+      case response
+        when Net::HTTPRedirection then
+          @response = post(response['location'], limit - 1)
+        else
+          @response = response
+      end
+
+      @response
+    end
   end
 end
