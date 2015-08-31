a02712a286b95b427cf52613781be9c42558ab3a
Ilja Bobkevic <ilja.bobkevic@unibet.com>
2015-08-31 18:51:10 +0200

    Bump version to 1.1.0

--- lib/go_api_client/version.rb
+++ lib/go_api_client/version.rb
@@ -1,7 +1,7 @@
 module GoApiClient
   module Version
     MAJOR = 1
-    MINOR = 0
+    MINOR = 1
     PATCH = 0
     RELEASE = ENV['GO_PIPELINE_COUNTER']
     VERSION = [MAJOR, MINOR, PATCH, RELEASE].compact.join('.')
